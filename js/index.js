170499bcdee4627e0f1cb2d648153a997cd21b21
Hamza Elhamdadi <irishlad98@vl965-172-31-165-163.wireless.umass.edu>
2023-04-25 14:51:05 -0400

    qualtrics code for bubbleview

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+var img = 'img/dataset5.png'
+// var clicks = [];
+
+function logClick(log) {
+    clicks.push(log);
+}
+
+function start(imgUrl, bubbleR, blurR, selector){
+    let canvasId = 'mainCanvas';
+
+    let qDiv = selector ? d3.select(`#${selector.questionId}`) : d3.select(`#main`)
+
+    let main_row = qDiv.append('div').attr('class', 'row')
+
+    let canvas = document.createElement('canvas');
+    canvas.id = canvasId;
+    canvas.width = 1000;
+    canvas.height = 800;
+
+    main_row.append('div')
+        .attr('class', 'mx-auto col-8')
+        .attr('id', 'canvasCol')
+
+    document.getElementById('canvasCol')
+    .append(canvas)
+
+    console.log(img)
+    
+    bv.setup(imgUrl, canvasId, bubbleR, blurR, logClick)
+    
+ }
+
+// window.onload = function(){
+//     start(img, 30, 70)
+//     // console.log('tests')
+    
+// }
